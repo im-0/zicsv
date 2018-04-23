e1762a4e12375f229b1f9d287c56d8c905545f9b
Ivan Mironov <mironov.ivan@gmail.com>
2018-04-23 23:44:28 +0300

    zicsv-tool select: Do not fail immediately on read error.

--- zicsv-tool/src/select.rs
+++ zicsv-tool/src/select.rs
@@ -4,6 +4,8 @@ use failure;
 
 use zicsv;
 
+use print_err;
+
 pub struct SelectOptions {
     pub ipv4: bool,
     pub ipv4_network: bool,
@@ -20,26 +22,34 @@ pub fn select<StreamWriter>(
 where
     StreamWriter: std::io::Write,
 {
+    let mut n_errors = 0usize;
+
     for record in reader.iter() {
-        let record = record?;
+        match record {
+            Ok(record) => for address in &record.addresses {
+                let selected = match *address {
+                    zicsv::Address::IPv4(_) => options.ipv4,
+                    zicsv::Address::IPv4Network(_) => options.ipv4_network,
+                    zicsv::Address::DomainName(_) => options.domain,
+                    zicsv::Address::WildcardDomainName(_) => options.wildcard_domain,
+                    zicsv::Address::URL(_) => options.url,
 
-        for address in &record.addresses {
-            let selected = match *address {
-                zicsv::Address::IPv4(_) => options.ipv4,
-                zicsv::Address::IPv4Network(_) => options.ipv4_network,
-                zicsv::Address::DomainName(_) => options.domain,
-                zicsv::Address::WildcardDomainName(_) => options.wildcard_domain,
-                zicsv::Address::URL(_) => options.url,
+                    // Do nothing on unknown type of address.
+                    _ => false,
+                };
 
-                // Do nothing on unknown type of address.
-                _ => false,
-            };
+                if selected {
+                    writeln!(writer, "{}", address)?;
+                }
+            },
 
-            if selected {
-                writeln!(writer, "{}", address)?;
-            }
+            Err(error) => {
+                n_errors += 1;
+                print_err::print_error(&error);
+            },
         }
     }
 
+    ensure!(n_errors == 0, "{} errors occur while reading list", n_errors);
     Ok(())
 }
